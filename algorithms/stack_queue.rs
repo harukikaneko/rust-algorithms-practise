054db7c6863aa8c9838e7717f4b05d59f39140ea
Haruki Kaneko <niku11298@gmail.com>
2024-02-24 16:38:22 +0900

    自前Queue

--- algorithms/stack_queue.rs
+++ algorithms/stack_queue.rs
@@ -13,12 +13,8 @@ impl Stack {
     }
 
     pub fn push(&mut self, value: usize) {
-        if !self.values.is_empty() {
-            self.values.insert(self.top + 1, value);
-            return self.top += 1;
-        }
-
         self.values.insert(self.top, value);
+        self.top += 1;
     }
 
     pub fn pop(&mut self) -> Option<usize> {
@@ -29,6 +25,55 @@ impl Stack {
         let value = self.values.get(target_index).copied();
         self.values.remove(target_index);
         self.top = target_index;
+        value
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.values.len() == 0
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct Queue {
+    values: Vec<usize>,
+    head: usize,
+    tail: usize,
+}
+
+impl Queue {
+    pub fn new() -> Self {
+        Queue {
+            values: [].to_vec(),
+            head: 0,
+            tail: 0,
+        }
+    }
+
+    pub fn push_front(&mut self, value: usize) {
+        self.values.insert(self.head, value);
+        self.head += 1;
+    }
+
+    pub fn push_back(&mut self, value: usize) {
+        self.values.insert(self.tail, value);
+        self.tail += 1;
+    }
+
+    pub fn pop_front(&mut self) -> Option<usize> {
+        if self.is_empty() {
+            return None;
+        }
+        let value = self.values.get(self.head).copied();
+        self.values.remove(self.head);
+        value
+    }
+
+    pub fn pop_back(&mut self) -> Option<usize> {
+        if self.is_empty() {
+            return None;
+        }
+        let value = self.values.get(self.tail).copied();
+        self.values.remove(self.tail);
         value
     }
 
@@ -80,5 +125,18 @@ mod tests {
         assert_eq!(stack.pop(), Some(2));
         assert_eq!(stack.pop(), Some(1));
         assert_eq!(stack.pop(), None)
+    }
+
+    #[test]
+    fn test_queue() {
+        let mut queue = Queue::new();
+        queue.push_back(1);
+        queue.push_back(2);
+        queue.push_back(3);
+
+        assert_eq!(queue.pop_front(), Some(1));
+        assert_eq!(queue.pop_front(), Some(2));
+        assert_eq!(queue.pop_front(), Some(3));
+        assert_eq!(queue.pop_front(), None);
     }
 }
